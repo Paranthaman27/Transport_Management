6a0dd50562d5ca44428a00532e053ff9b1c9a344
Paranthaman27 <110550552+Paranthaman27@users.noreply.github.com>
2025-04-28 10:37:17 +0530

    Add files via upload

---
+++ Helpers/Filters/RoleAuthorizeAttribute.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Transport_Management.Interface;
+using Transport_Management.Models.DTO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Transport_Management.Helpers.Filters
+{
+    public class RoleAuthorizeAttribute : ActionFilterAttribute
+    {
+        private readonly string[] _roles;
+
+        public RoleAuthorizeAttribute(params string[] roles)
+        {
+            _roles = roles;
+        }
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            var session = context.HttpContext.Session;
+            var role = session.GetString("Role");
+            var rurl = context.HttpContext.Request.Path;
+            if (role == null || !_roles.Contains(role))
+            {
+                //var returnUrl = rurl; //context.HttpContext.Request.Path;
+                //var accessDeniedUrl = $"{returnUrl}&accessDenied=true";
+
+                context.Result = new RedirectToActionResult("Index", "Home", null);
+                //context.HttpContext.Items["AccessDenied"] = true;
+            }
+            else
+            {
+                base.OnActionExecuting(context);
+            }
+        }
+    }
+}
---
+++ Helpers/Filters/SessionAuthorizeAttribute.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Transport_Management.Interface;
+using Transport_Management.Models.DTO;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Transport_Management.Helpers.Filters
+{
+    public class SessionAuthorizeAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Session.GetString("userSessionId") == null)
+            {
+                context.Result = new RedirectToActionResult("Login", "Auth", null);
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}

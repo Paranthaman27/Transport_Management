a2363a86df9b218321b2f2d73225fc1256aeaa09
Paranthaman27 <110550552+Paranthaman27@users.noreply.github.com>
2025-04-28 10:42:51 +0530

    Add files via upload

---
+++ Models/DTO/ApiResponseDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Transport_Management.Models.DTO
+{
+    public class ApiResponseDTO
+    {
+        public bool success {  get; set; }
+        public string message {  get; set; }
+        public int statusCode {  get; set; }
+        public dynamic data {  get; set; }
+    }
+}
---
+++ Models/DTO/userDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Transport_Management.Models.DTO
+{
+    public class userDTO
+    {
+        public string userEmail { get; set; }
+        public string userPassword { get; set; }
+    }
+}

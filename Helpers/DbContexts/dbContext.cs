e1fd950d19124c9800f79b11a1381b1b218741d2
Paranthaman27 <110550552+Paranthaman27@users.noreply.github.com>
2025-04-28 10:36:08 +0530

    Add files via upload

---
+++ Helpers/DbContexts/dbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Transport_Management;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Transport_Management.Models.Entity;
+
+namespace Transport_Management.Helpers.DbContexts
+{
+    public class dbContext : DbContext
+    {
+        public dbContext(DbContextOptions<dbContext> options) : base(options) { }
+        public DbSet<mstUser> mstUsers { get; set; }
+        public DbSet<mstUserType> mstUserType { get; set; }
+        public DbSet<mstVehicle> mstVehicle { get; set; }
+        public DbSet<mstCompany> mstCompany { get; set; }
+        public DbSet<mstRentalEntry> mstRentalEntry { get; set; }
+        public DbSet<mstInvoice> mstInvoice { get; set; }
+
+
+    }
+}
